82bccb27e555c9282b72b9a889c2bef6b9ea0841
Арсений Пермяков <arseniy.permyakov@gmail.com>
2023-04-13 17:58:53 +0300

    added filter

--- src/components/todo-list/index.jsx
+++ src/components/todo-list/index.jsx
@@ -1,17 +1,30 @@
 import { useSelector } from "react-redux";
 import { todosSelector } from "../../store/selectors/todo";
 import { Todo } from "../todo";
+import { useState } from "react";
 
-import styles from './index.module.css';
+import styles from "./index.module.css";
 
 export const TodoList = () => {
+  const [filter, setFilter] = useState("active");
   const todos = useSelector(todosSelector);
 
+  const filteredTodos =
+    filter === "active"
+      ? todos.filter((todo) => !todo.completed)
+      : todos.filter((todo) => todo.completed);
+
   return (
-    <ul className={styles.list}>
-      {todos.map((todo) => (
-        <Todo key={todo.id} todo={todo} />
-      ))}
-    </ul>
+    <div>
+      <div>
+        <button onClick={() => setFilter("active")}>Active</button>
+        <button onClick={() => setFilter("completed")}>Completed</button>
+      </div>
+      <ul className={styles.list}>
+        {filteredTodos.map((todo) => (
+          <Todo key={todo.id} todo={todo} />
+        ))}
+      </ul>
+    </div>
   );
 };
